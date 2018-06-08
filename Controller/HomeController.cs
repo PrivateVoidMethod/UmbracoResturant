95e0e4c394667c8a6a98b929f990b1c0c16853a5
Kasper <Kasperjohnsen@live.dk>
2018-06-08 19:22:37 +0200

    inital commit

---
+++ Controller/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace KaspersBurger.Controller
+{
+    public class HomeController : SurfaceController
+    {
+        private const string partialPath = "~/Views/Partials/Home/";
+
+        public ActionResult RenderSpecials()
+        {
+            return PartialView(partialPath + "_Specials.cshtml");
+        }
+
+        public ActionResult RenderAbout()
+        {
+            return PartialView(partialPath + "_About.cshtml");
+        }
+
+        public ActionResult RenderMenu()
+        {
+            return PartialView(partialPath + "_Menu.cshtml");
+        }
+
+        public ActionResult RenderGallery()
+        {
+            return PartialView(partialPath + "_Gallery.cshtml");
+        }
+
+        public ActionResult RenderChef()
+        {
+            return PartialView(partialPath + "_Chef.cshtml");
+        }
+
+        public ActionResult RenderContact()
+        {
+            return PartialView(partialPath + "_Contact.cshtml");
+        }
+    }
+}
---
+++ Controller/SiteLayoutController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace KaspersBurger.Controller
+{
+    public class SiteLayoutController : SurfaceController
+    {
+        private const string partialPath = "~/Views/Partials/SiteLayout/";
+
+        public ActionResult RenderHeader()
+        {
+            return PartialView(partialPath + "_Header.cshtml");
+        }
+        public ActionResult RenderFooter()
+        {
+            return PartialView(partialPath + "_Footer.cshtml");
+        }
+    }
+}
